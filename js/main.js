6df426a9248b5d50f718dca97e4ca6150ecd4df6
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-29 15:49:11 -0700

    Displayed the correct and accuracy and wpm stats in their respective places.

--- js/main.js
+++ js/main.js
@@ -46,29 +46,42 @@ function timer() {
     clearInterval(intervalId);
     var $stats = document.querySelector('div.stats');
     $stats.classList.toggle('hidden');
+    var $accuracy = document.querySelector('p.accuracy');
+    var $wpm = document.querySelector('p.wpm');
+    var $correctCharacters = document.querySelectorAll('span.correct');
+    var $incorrectCharacters = document.querySelectorAll('span.incorrect');
+    var minutes = seconds / 60;
+    var grossWPM = (($characters.length / 5) - $incorrectCharacters.length) / minutes;
+    $accuracy.textContent = `${Math.round((($correctCharacters.length + 1) / $characters.length) * 100)}%`;
+    $wpm.textContent = `${Math.round(grossWPM)}`;
   }
 }
 
 $webPage.addEventListener('keydown', function (event) {
   var $characters = document.querySelectorAll('span.letter');
-  if (event.key === $characters[currentCharacter].textContent) {
-    $characters[currentCharacter].classList.toggle('correct');
-    $characters[currentCharacter].classList.toggle('current-character');
-    currentCharacter++;
-    $characters[currentCharacter].classList.toggle('current-character');
-  } else if (event.key === 'Backspace' && (currentCharacter + 1 !== $characters.length && currentCharacter !== 0)) {
-    $characters[currentCharacter].className = 'letter';
-    currentCharacter--;
-    $characters[currentCharacter].className = 'letter current-character';
-  } else if (event.key.length < 2) {
-    $characters[currentCharacter].classList.toggle('incorrect');
-    $characters[currentCharacter].classList.toggle('current-character');
-    currentCharacter++;
-    $characters[currentCharacter].classList.toggle('current-character');
-  }
-  if ($characters[currentCharacter].textContent !== ' ') {
-    var $currentWord = $characters[currentCharacter].closest('span.word');
-    $currentWord.className = 'word active';
+  if ($characters.length !== currentCharacter + 1) {
+    if (event.key === $characters[currentCharacter].textContent) {
+      $characters[currentCharacter].classList.toggle('correct');
+      $characters[currentCharacter].classList.toggle('current-character');
+      currentCharacter++;
+      $characters[currentCharacter].classList.toggle('current-character');
+    } else if (event.key === 'Backspace' && (currentCharacter + 1 !== $characters.length && currentCharacter !== 0)) {
+      $characters[currentCharacter].className = 'letter';
+      if ($characters[currentCharacter].textContent !== ' ') {
+        $characters[currentCharacter].closest('span.word').className = 'word';
+      }
+      currentCharacter--;
+      $characters[currentCharacter].className = 'letter current-character';
+    } else if (event.key.length < 2) {
+      $characters[currentCharacter].classList.toggle('incorrect');
+      $characters[currentCharacter].classList.toggle('current-character');
+      currentCharacter++;
+      $characters[currentCharacter].classList.toggle('current-character');
+    }
+    if ($characters[currentCharacter].textContent !== ' ') {
+      var $currentWord = $characters[currentCharacter].closest('span.word');
+      $currentWord.className = 'word active';
+    }
   }
   if (event.key === 'Tab') {
     $p.textContent = '';
